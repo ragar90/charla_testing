581a6cbee305b80ecc028945db6258d75a9a15f5
Rene Garcia <rene.garciah90@gmail.com>
2018-03-10 00:03:38 -0600

    Clase 0

--- kinematic/index.js
+++ kinematic/index.js
@@ -1,12 +1,3 @@
-function distancia1(vi, a, t) {
-  if (vi === null || vi === undefined)
-    return null;
-  if (a === null || a === undefined )
-    return null
-  if (t === null || t === undefined)
-    return null
-  return vi * t + ((1 / 2) * a * t);
-}
 module.exports = {
   distancia1: distancia1
 }
--- specs/kinimatic.test_unit.js
+++ specs/kinimatic.test_unit.js
@@ -7,21 +7,9 @@ const should = chai.should();
 describe('Modulo Kinematic para funciones de cinematica', function () {
   context('Funcion de distancia en base a velocidad inical, aceleracion y tiempo', function () {
     it('SHOULD return the correct value for the supplied parammetters', function () {
-      const vi = 0;
-      const a = 10;
-      const t = 10;
-      const distancia = kinematic.distancia1(vi, a, t);
-      assert.equal(distancia, 50);
-      expect(distancia).to.equal(50);
-      distancia.should.equal(50);
     });
 
     it('SHOUlD return null for missing parrametters', function () {
-      const vi = 0;
-      const a = 10;
-      const distancia = kinematic.distancia1(vi, a);
-      assert.strictEqual(distancia, null);
-      expect(distancia).to.equal(null);
     })
   })
 })
